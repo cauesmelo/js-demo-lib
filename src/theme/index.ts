41818431bd963308de4bb004220d3d6337015667
cauesmelo <Melo.Caue@bcg.com>
2025-02-20 21:32:53 -0300

    fix: reduce secondary text font transparency

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -264,7 +264,7 @@ export const lightTheme = createTheme({
     },
     text: {
       primary: "#000000",
-      secondary: "rgba(0, 0, 0, 0.8)",
+      secondary: "rgba(0, 0, 0, 0.7)",
     },
   },
 });
